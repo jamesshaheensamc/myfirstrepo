76d3f72e27b45bd7882c15212f2e93864baa5246
jamesshaheen <jamesshaheen@situsamc.com>
2022-09-08 07:53:27 -0700

    Added dev file

--- file1.ps1
+++ file1.ps1
@@ -1,6 +1,11 @@
+iex "git checkout -b dev2"
+
 for ($i = 1; $i -lt 4; $i++)
 {
-    New-Item "file$i.txt"
-    iex  "git add ."
-    iex  "git commit -m 'Added file'"
-    }
+New-Item "dev_2_$i.txt"
+iex  "git add ."
+iex  "git commit -m 'Added dev file'"
+}
+
+iex "git checkout master"
+iex "git merge dev2 --no-ff"
